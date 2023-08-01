5db7e85bccbf51bb3c929c8b5a98971d8e163b8b
do7be <bomsap700@gmail.com>
2023-08-01 12:36:46 +0900

    家族絵文字を分解してみたけど、そもそもterminalに分解前が表示できてなかったから意味なかった

--- src/char_for_emoji/main.rs
+++ src/char_for_emoji/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let chars = "🦀👨‍👩‍👧‍👦🍅".chars().collect::<Vec<char>>();
-    println!("{}", chars.len());
-    println!("{}", chars[0]);
-    println!("{:?}", chars);
+    let original = "🦀👨‍👩‍👧‍👦🍅";
+    let chars = original.chars().collect::<Vec<char>>();
+    println!("original: {}", original);
+    println!("length: {}", chars.len());
+    println!("first: {}", chars[0]);
+    println!("list: {:?}", chars);
+
+    let filtered: String = chars
+        .clone()
+        .iter()
+        .filter(|&&char| char != '\u{200d}')
+        .collect();
+
+    println!("disassembled: {}", filtered);
 }
